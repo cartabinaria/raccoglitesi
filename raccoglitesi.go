99d1480d8bc352bb1f755073ceb58eaab3bec013
VaiTon <eyadlorenzo@gmail.com>
2025-06-16 23:56:10 +0200

    Add REUSE information
    <https://reuse.software/spec-3.3/>

--- raccoglitesi.go
+++ raccoglitesi.go
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2023 Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
+// SPDX-FileCopyrightText: 2023 - 2024 Stefano Volpe <foxy@teapot.ovh>
+// SPDX-FileCopyrightText: 2023 Eyad Issa <eyadlorenzo@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package main
 
 import (
